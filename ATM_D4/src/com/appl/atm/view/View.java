4b52952547ed43b54e8a4a9f17db54db5ff06680
ichacahyawulan <45441252+ichacahyawulan@users.noreply.github.com>
2019-05-17 10:05:25 +0700

    edit class keypad sama view

--- ATM_D4/src/com/appl/atm/view/View.java
+++ ATM_D4/src/com/appl/atm/view/View.java
@@ -10,5 +10,27 @@ package com.appl.atm.view;
  * @author IchaCahyaWulan
  */
 public class View {
+    Screen screen = new Screen();
+    Keypad keypad = new Keypad();
+    
+    public String inputRecipientAccountNumberV(){
+        String accountNumber;
+        screen.displayMessage("\nPlease enter the recipient's account number : ");
+        accountNumber = keypad.getStr();
+        return accountNumber;
+    }
+    
+    public double inputTheAmountV(){
+        double Amount;
+        screen.displayMessage("\nPlease enter the amount (0 to cancel): ");
+        Amount = keypad.getInput();
+        return Amount;
+    }
+    
+    public void displayMaxOneTimeLimitV(int limit){
+        screen.displayMessage("\nThe maximum limit for 1x transfer is ");
+        screen.displayDollarAmount(limit);
+    }
+    
     
 }
