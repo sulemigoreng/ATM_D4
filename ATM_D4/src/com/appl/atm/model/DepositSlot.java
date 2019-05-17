922a12088e9aaa5530ff28adc9d025df78faf8ba
RomaUlinaP <48423381+RomaUlinaP@users.noreply.github.com>
2019-05-17 11:23:29 +0700

    Update DepositSlot.java
    +public boolean deleteList();

--- ATM_D4/src/com/appl/atm/model/DepositSlot.java
+++ ATM_D4/src/com/appl/atm/model/DepositSlot.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package com.appl.atm.model;
+import java.util.HashMap;
+import com.appl.atm.model.Customer;
 
 /**
  *
@@ -13,4 +15,14 @@ public class DepositSlot {
     public boolean isEnvelopeReceived() {
 	return true; // deposit envelope was received
     } 
+    public boolean deleteList(HashMap envelopes, Customer theCustomer, double amount){
+        if(envelopes.containsKey(theCustomer)){
+            double balance = theCustomer.getAvailableBalance();
+            theCustomer.setAvailableBalance(balance+amount);
+            envelopes.remove(theCustomer);
+            return true;
+        }else{
+            return false;
+        }   
+    }
 }
