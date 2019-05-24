03b7a9f01ff8b79e79ee1618f4134e7a3633744e
Evan Lokajaya <48423337+lokajayae@users.noreply.github.com>
2019-05-24 10:53:48 +0700

    Menambahkan class BankStatementController dan menambahkan contoh implementasi bankStatement pada class WithdrawalController

---
+++ ATM_D4/src/com/appl/atm/controller/BankStatementController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appl.atm.controller;
+
+import com.appl.atm.view.Keypad;
+import com.appl.atm.view.Screen;
+import com.appl.atm.model.Customer;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BankStatementController extends TransactionController {
+    Customer customer;
+
+    public BankStatementController(Keypad theKeypad, Screen theScreen, Customer customer) {
+        super(theKeypad, theScreen);
+        this.customer=customer;
+    }
+    
+    
+    public void addLog(String accountNumber, int amount, String Transaction){
+        customer.getTransaksiLog().add("[DATE] "+Transaction+" Account Number "+accountNumber+" with amount"+amount);
+    }
+
+    @Override
+    public int run() {
+        return 0;
+    }
+}
