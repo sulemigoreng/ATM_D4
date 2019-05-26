024a41c985c594f74524d45e1a74afdc8f6a3437
Rayhan Anandhias <rayhananandhias@gmail.com>
2019-05-26 19:51:56 +0700

    Add Daily Limit Variable in Bisnis

--- ATM_D4/src/com/appl/atm/model/Bisnis.java
+++ ATM_D4/src/com/appl/atm/model/Bisnis.java
@@ -3,6 +3,8 @@ package com.appl.atm.model;
 public class Bisnis extends Customer {
     private static double MAX_WITHDRAWAL = 2000;
     private static double MAX_TRANSFER   = 10000;
+    private static double MAX_DAILY_WITHDRAWAL = 10000;
+    private static double MAX_DAILY_TRANSFER =  40000;
 
     public Bisnis(int theAccountNumber, int thePIN, double theAvailableBalance, double theTotalBalance) {
         super(theAccountNumber, thePIN, theAvailableBalance, theTotalBalance);
@@ -21,5 +23,13 @@ public class Bisnis extends Customer {
     @Override
     public boolean isBisnis(){
         return true;
+    }
+
+    public static double getMAX_DAILY_WITHDRAWAL() {
+        return MAX_DAILY_WITHDRAWAL;
+    }
+
+    public static double getMAX_DAILY_TRANSFER() {
+        return MAX_DAILY_TRANSFER;
     }
 }
