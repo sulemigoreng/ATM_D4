f640e8ddfe505f302068197e328c0d89f163bb5e
adevaraz <zaraveda.zv@gmail.com>
2019-05-29 22:38:46 +0700

    create CheckCashDispenserController

--- ATM_D4/src/com/appl/atm/controller/CheckCashDispenserController.java
+++ ATM_D4/src/com/appl/atm/controller/CheckCashDispenserController.java
@@ -5,16 +5,35 @@
  */
 package com.appl.atm.controller;
 
+import com.appl.atm.model.CashDispenser;
+import com.appl.atm.view.Keypad;
+import com.appl.atm.view.Screen;
+
 /**
  *
  * @author Zara Veda
  */
 public class CheckCashDispenserController extends TransactionController {
-
+    Keypad keypad;
+    Screen screen;
+    CashDispenser cashDispenser;
+    
+    public CheckCashDispenserController(CashDispenser theCashDispenser,
+        Keypad theKeypad, Screen theScreen) {
+        super(theKeypad, theScreen);
+        
+        cashDispenser = theCashDispenser;
+    }
+    
     @Override
     public int run() {
+        screen = getScreen();
+        keypad = getKeypad();
         
-        
+        screen.displayMessageLine("Amount in dispenser : ");
+        screen.displayDollarAmount(cashDispenser.getAmountCashDispenser());
+        screen.displayMessageLine("\n" + "Lots of money : " +
+            cashDispenser.getAmountCashDispenser()/20);
         return 0;
     }
     
