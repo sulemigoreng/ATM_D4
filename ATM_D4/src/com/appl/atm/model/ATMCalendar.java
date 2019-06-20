8324f9ce6abd163e8ff996090269defe7e462eb4
Raefaldhi Amartya Junior <raefaldhiamartya@gmail.com>
2019-06-21 03:08:46 +0700

    Refactor tanggal

---
+++ ATM_D4/src/com/appl/atm/model/ATMCalendar.java
@@ -0,0 +1,41 @@
+package com.appl.atm.model;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class ATMCalendar {
+    private final Calendar calendar;
+    private long startMillisecond;
+    
+    public ATMCalendar() {
+        calendar = new GregorianCalendar();
+        startMillisecond = System.currentTimeMillis();
+    }
+    
+    private void refresh() {
+        try {
+            Thread.sleep(1);
+            
+            // renew startMillisecond
+            long endMillisecond = startMillisecond;
+            startMillisecond = System.currentTimeMillis();
+
+            // renew calendar date to latest machine date
+            calendar.setTimeInMillis(
+                    calendar.getTimeInMillis() 
+                            + (startMillisecond - endMillisecond));                    
+        } catch(InterruptedException e) {
+            // TODO
+        }
+    }
+    
+    public Calendar getCalendar() {
+        refresh();
+        return (Calendar)calendar.clone();
+    }
+    
+    public void addDate(){
+        refresh();
+        calendar.add(Calendar.DATE, 1);
+    }
+}
